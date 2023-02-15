1a54662d7319f7337125d9475ae1e9db1d20b955
Brian Nichols <brian.p.nichols@gmail.com>
2023-02-15 15:24:10 -0600

    Adding Google Provider configuration

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
+provider "google" {
+    project = "sigma-firmament-375715"
+    region = "us-central1"
+}
+
 resource "google_storage_bucket" "static-site" {
 	name	= "bnichols.xyz"
-	location = "us-east1"
+	location = "us-central1"
 	force_destroy = true
 
 	uniform_bucket_level_access = true
